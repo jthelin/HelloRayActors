7fe1552b3fb9d027e789d476664860bf8f83cfad
Jorgen Thelin <jthelin@microsoft.com>
2019-05-24 11:48:01 -0700

    Initial environment check-in.

---
+++ __init__.py
@@ -0,0 +1,12 @@
+__version__ = "1.0.0.dev0"
+
+__title__ = "HelloRayActors"
+__description__ = "A simple example of distributed actors using ray"
+__url__ = "https://github.com/jthelin/"
+__doc__ = __description__ + " <" + __url__ + ">"
+
+__author__ = "Jorgen Thelin"
+__email__ = "jthelin@microsoft.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2019 Jorgen Thelin"
